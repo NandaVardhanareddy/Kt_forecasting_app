b891bb91cd6055e256a36cebc6a15ee701f8ccf6
NandaVardhanareddy <108464880+NandaVardhanareddy@users.noreply.github.com>
2023-08-05 10:21:36 +0530

    Update kt_app.py

--- kt_app.py
+++ kt_app.py
@@ -73,8 +73,22 @@ def main():
     # Custom CSS for positioning the logo at the right top corner
     
 
-    # Render the logo at the right top corner
-   
+    # forecasting
+    st.write('Enter the past 5 days clearness index values:')
+    d5 = st.number_input('Enter a kt value between 0 and 1(day -5)', step=0.01)
+    d4 = st.number_input('Enter a kt value between 0 and 1(day -4)', step=0.01)
+    d3 = st.number_input('Enter a kt value between 0 and 1(day -3)', step=0.01)
+    d2 = st.number_input('Enter a kt value between 0 and 1(day -2)', step=0.01)
+    d1 = st.number_input('Enter a kt value between 0 and 1(day -1)', step=0.01)
+    input_sequence = np.array([d5, d4, d3, d2, d1]).reshape(5, 1)
+    forecast_days = st.slider("Select the number of forecast days", 0, 15, value=5)
+    if st.button('Forecast'):
+        forecasted_values = make_forecast(input_sequence,model, forecast_days)
+        st.write(f"Forecasted Clearness Index for the next {forecast_days} days:")
+        df = pd.DataFrame({'Day': np.arange(1, forecast_days + 1), 'Clearness Index': forecasted_values})
+        st.dataframe(df)
+    
+    kt = forecasted_values[0]
     # taking input for calculations
 
     latitude = st.number_input('LATITUDE', step=0.01)
@@ -99,7 +113,7 @@ def main():
 
     # Radiation calculations
     p = 1370*(1 + 0.034*m.cos(m.radians(360*julian_day/365))) #Extra Terrestrial Solar Insolation
-    gd = p*m.exp((-1)*od*a) #Clear Sky Radiation
+    gd = kt*p #Clear Sky Radiation
     aoi = m.cos(m.radians(se))*m.cos(m.radians(wall_azimuth_angle)) #Angle of incidence
     br = gd*aoi #Beam Radiation
     dr = gd*sdf #Diffused Radiation
@@ -108,7 +122,7 @@ def main():
     te = tr_d*wall_area #Total Energy
 
     #for a wall
-    brw = gd*(m.cos(m.radians(surface_slope))+m.tan(m.radians(se))*m.cos(m.radians(surface_orientation))*m.sin(m.radians(surface_slope)))
+    brw = (gd/3) + (gd*m.cos(wall_azimuth_angle)) + (gd/10)*m.cos(m.radians(360*(julian_day - 172)/365))
     trw = (brw*4.814 + dr*10)*wall_area
 
     if st.button('Results'):
@@ -121,8 +135,8 @@ def main():
 
         # Create DataFrame for the second table
         table2_data = {
-            "Variable": ["Extra Terrestrial Solar Insolation", "Clear Sky Radiation", "Angle of incidence", "Beam Radiation", "Diffused Radiation", "avg Peak Sun Hours", "Total Radiation Per Day", "Total Energy",'Beam Radiation for a wall','Total Radiation per day for a wall'],
-            "Value": [p, gd, aoi, br, dr, avg_pk_hr, tr_d, te,brw,trw]
+            "Variable": ["Extra Terrestrial Solar Insolation", "Clear Sky Radiation","Beam Radiation", "Diffused Radiation",'Beam Radiation on a wall','Total Radiation on a wall'],
+            "Value": [p, gd,br, dr,brw,trw]
         }
 
         table2_df = pd.DataFrame(table2_data)
@@ -134,20 +148,7 @@ def main():
         st.table(table2_df)
 
 
-    st.write('Enter the past 5 days clearness index values:')
-    d5 = st.number_input('Enter a kt value between 0 and 1(day -5)', step=0.01)
-    d4 = st.number_input('Enter a kt value between 0 and 1(day -4)', step=0.01)
-    d3 = st.number_input('Enter a kt value between 0 and 1(day -3)', step=0.01)
-    d2 = st.number_input('Enter a kt value between 0 and 1(day -2)', step=0.01)
-    d1 = st.number_input('Enter a kt value between 0 and 1(day -1)', step=0.01)
-    input_sequence = np.array([d5, d4, d3, d2, d1]).reshape(5, 1)
-    forecast_days = st.slider("Select the number of forecast days", 0, 15, value=5)
-    if st.button('Forecast'):
-        forecasted_values = make_forecast(input_sequence,model, forecast_days)
-        st.write(f"Forecasted Clearness Index for the next {forecast_days} days:")
-        df = pd.DataFrame({'Day': np.arange(1, forecast_days + 1), 'Clearness Index': forecasted_values})
-        st.dataframe(df)
-    
+   
 
 
 if __name__ == '__main__':
